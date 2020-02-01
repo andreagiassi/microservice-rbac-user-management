7cfce06e9566572dc554aa03261b271017bbc9d4
Andrea Giassi <andrea.giassi@travelaer.com>
2020-02-01 18:01:54 +0000

    first impl

---
+++ src/main/java/com/giassi/microservice/demo2/Microdemo2Application.java
@@ -0,0 +1,13 @@
+package com.giassi.microservice.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microdemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/giassi/microservice/demo2/Microdemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giassi.microservice.demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microdemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
