e5f31bb20cefb83290319430f89b75a616f412ac
Andrea Giassi <andrea.giassi@travelaer.com>
2020-02-03 22:29:49 +0000

    Echo on db.cmd

--- db.cmd
+++ db.cmd
@@ -1,2 +1,4 @@
+echo "Pulling latest MySql database..."
 docker pull mysql
+echo "Running MySql..."
 docker run --name mysql8.0 -v /Users/andreagiassi/dev/mysql_data/8.0:/var/lib/mysql -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=root mysql:8.0
