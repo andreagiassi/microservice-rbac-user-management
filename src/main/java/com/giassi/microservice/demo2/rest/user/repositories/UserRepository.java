8c4ced4cb5bf05959d7af8852e71161fa2714bb6
Andrea Giassi <andrea.giassi@travelaer.com>
2020-02-04 18:10:18 +0000

    Marked UserRepository as a @Repository

--- src/main/java/com/giassi/microservice/demo2/rest/user/repositories/UserRepository.java
+++ src/main/java/com/giassi/microservice/demo2/rest/user/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.giassi.microservice.demo2.rest.user.repositories;
 
 import com.giassi.microservice.demo2.rest.user.entities.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByEmail(String email);
