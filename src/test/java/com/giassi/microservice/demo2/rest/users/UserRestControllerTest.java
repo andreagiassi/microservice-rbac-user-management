c50861d1d5bfcbb83b1f6ecb12577a0577c98490
Andrea Giassi <andrea.giassi@travelaer.com>
2020-02-14 17:27:44 +0000

    Test on created user

--- src/test/java/com/giassi/microservice/demo2/rest/users/UserRestControllerTest.java
+++ src/test/java/com/giassi/microservice/demo2/rest/users/UserRestControllerTest.java
@@ -1,18 +1,25 @@
 package com.giassi.microservice.demo2.rest.users;
 
+import com.giassi.microservice.demo2.rest.users.dtos.AddressDTO;
+import com.giassi.microservice.demo2.rest.users.dtos.CreateOrUpdateUserDTO;
+import com.giassi.microservice.demo2.rest.users.dtos.RoleDTO;
 import com.giassi.microservice.demo2.rest.users.dtos.UserDTO;
+import com.giassi.microservice.demo2.rest.users.entities.Role;
+import com.giassi.microservice.demo2.rest.users.services.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.URI;
+
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -20,6 +27,9 @@ public class UserRestControllerTest {
 
     @Autowired
     TestRestTemplate restTemplate;
+
+    @Autowired
+    UserService userService;
 
     @Test
     public void test_getUserById() {
@@ -35,9 +45,58 @@ public class UserRestControllerTest {
 
         assertThat(userDTO.getId(), equalTo(1L));
         assertThat(userDTO.getName(), equalTo("Andrea"));
-        assertThat(userDTO.getSurname(), equalTo("Giassi"));
+        assertThat(userDTO.getSurname(), equalTo("Test"));
         assertThat(userDTO.getEmail(), equalTo("andrea.test@gmail.com"));
         assertThat(userDTO.isEnabled(), equalTo(true));
+    }
+
+    @Test
+    public void test_createUser() {
+        CreateOrUpdateUserDTO createOrUpdateUserDTO = CreateOrUpdateUserDTO.builder()
+               .username("test1")
+               .name("Marco")
+               .surname("Blu")
+               .gender("MALE")
+               .enabled(true)
+               .roleId(Role.USER)
+               .note("created for test1")
+               .email("marco.blu@gmail.com")
+               .phone("+3531194334455")
+               .address("dark road 1")
+               .city("Dublin")
+               .country("Ireland")
+               .zipCode("47335").build();
+
+        URI uri = URI.create("/users");
+
+        HttpEntity<CreateOrUpdateUserDTO> request = new HttpEntity<>(createOrUpdateUserDTO);
+        UserDTO userDTO = restTemplate.postForObject(uri, request, UserDTO.class);
+
+        assertNotNull(userDTO);
+        assertEquals("test1", userDTO.getUsername());
+        assertEquals("Marco", userDTO.getName());
+        assertEquals("Blu", userDTO.getSurname());
+        assertEquals("MALE", userDTO.getGender());
+
+        RoleDTO roleDTO = userDTO.getRoleDTO();
+        assertNotNull(roleDTO);
+        assertEquals(Long.valueOf(1) ,roleDTO.getId());
+        assertEquals("USER", roleDTO.getRole());
+
+        assertEquals(true, userDTO.isEnabled());
+        assertEquals("created for test1", userDTO.getNote());
+        assertEquals("marco.blu@gmail.com", userDTO.getEmail());
+        assertEquals("+3531194334455", userDTO.getPhone());
+
+        assertNotNull(userDTO.getAddressDTO());
+        AddressDTO addressDTO = userDTO.getAddressDTO();
+        assertEquals("dark road 1", addressDTO.getAddress());
+        assertEquals("Dublin", addressDTO.getCity());
+        assertEquals("Ireland", addressDTO.getCountry());
+        assertEquals("47335", addressDTO.getZipCode());
+
+        // delete the created user
+        userService.deleteUserById(userDTO.getId());
     }
 
 }
