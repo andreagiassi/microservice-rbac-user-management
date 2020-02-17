27de50652434e3632836d547eaf9f75324caf625
Andrea Giassi <andrea.giassi@travelaer.com>
2020-02-17 16:14:39 +0000

    Set HttpStatus not found on RoleNotFoundException

--- src/main/java/com/giassi/microservice/demo2/rest/users/exceptions/RoleNotFoundException.java
+++ src/main/java/com/giassi/microservice/demo2/rest/users/exceptions/RoleNotFoundException.java
@@ -3,7 +3,7 @@ package com.giassi.microservice.demo2.rest.users.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class RoleNotFoundException extends RuntimeException {
 
 }
