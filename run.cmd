5dfd58387352427c6ad145ff2d3b776a625c0796
Andrea Giassi <andrea.giassi@travelaer.com>
2020-02-23 16:56:27 +0000

    Use the dev-network on docker run

--- run.cmd
+++ run.cmd
@@ -4,4 +4,4 @@ echo "Dockerizing MicroserviceDemo2..."
 docker rm micro2
 docker build -t micro2 .
 echo "Running the Docker container for micro2..."
-docker run --name micro2 -p 8090:8090 micro2
+docker run --name micro2 --network dev-network -p 8090:8090 micro2
