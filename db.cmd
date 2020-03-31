63825475fb9c2d270607c014bf01afde709d6614
Andrea Giassi <andrea.giassi@travelaer.com>
2020-03-31 12:00:32 +0100

    set network on the db.cmd

--- db.cmd
+++ db.cmd
@@ -1,4 +1,4 @@
 echo "Pulling latest MySql database..."
 docker pull mysql
 echo "Running MySql..."
-docker run --name mysql8.0 -v /Users/andreagiassi/dev/mysql_data/8.0:/var/lib/mysql -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=root mysql:8.0
+docker run --name mysql8.0 --network dev-network -v /Users/andreagiassi/dev/mysql_data/8.0:/var/lib/mysql -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=root mysql:8.0
