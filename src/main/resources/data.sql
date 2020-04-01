0dd8eb466a2405e191cb587d9618f171968a1d73
Andrea Giassi <andrea.giassi@travelaer.com>
2020-04-01 20:16:47 +0100

    Fix on data.sql and set not enabled user id = 6

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,10 +12,12 @@ INSERT INTO users(id, username, password, salt, name, surname, gender, role_id) 
 INSERT INTO users(id, username, password, salt, name, surname, gender, role_id) VALUES (2, 'mario', '1d/NZaEqNgtEomytAPrwm/+QjmbudLg33oeEk77Xh88=', 'WZeBXmCI9cAz3LyY9Sdllj9l5iPsXC', 'Mario', 'Rossi', 0, 1);
 INSERT INTO users(id, username, password, salt, name, surname, gender, role_id) VALUES (3, 'stefania', '1d/NZaEqNgtEomytAPrwm/+QjmbudLg33oeEk77Xh88=', 'WZeBXmCI9cAz3LyY9Sdllj9l5iPsXC', 'Stefania', 'Verdi', 1, 1);
 INSERT INTO users(id, username, password, salt, name, surname, gender, role_id) VALUES (4, 'veronica', '1d/NZaEqNgtEomytAPrwm/+QjmbudLg33oeEk77Xh88=', 'WZeBXmCI9cAz3LyY9Sdllj9l5iPsXC', 'Veronica', 'Gialli', 1, 1);
-INSERT INTO users(id, username, password, salt, name, surname, gender, role_id) VALUES (5, 'mark', '1d/NZaEqNgtEomytAPrwm/+QjmbudLg33oeEk77Xh88=', 'Mark', 'WZeBXmCI9cAz3LyY9Sdllj9l5iPsXC', 'Green', 0, 1);
-INSERT INTO users(id, username, password, salt, name, surname, gender, role_id) VALUES (6, 'paul', '1d/NZaEqNgtEomytAPrwm/+QjmbudLg33oeEk77Xh88=', 'Paul', 'WZeBXmCI9cAz3LyY9Sdllj9l5iPsXC', 'Ludwing', 0, 1);
+INSERT INTO users(id, username, password, salt, name, surname, gender, role_id) VALUES (5, 'mark', '1d/NZaEqNgtEomytAPrwm/+QjmbudLg33oeEk77Xh88=', 'WZeBXmCI9cAz3LyY9Sdllj9l5iPsXC', 'Mark', 'Green', 0, 1);
+INSERT INTO users(id, username, password, salt, name, surname, gender, role_id) VALUES (6, 'paul', '1d/NZaEqNgtEomytAPrwm/+QjmbudLg33oeEk77Xh88=', 'WZeBXmCI9cAz3LyY9Sdllj9l5iPsXC', 'Paul', 'Ludwing', 0, 1);
 INSERT INTO users(id, username, password, salt, name, surname, gender, role_id) VALUES (7, 'jennifer', '1d/NZaEqNgtEomytAPrwm/+QjmbudLg33oeEk77Xh88=', 'WZeBXmCI9cAz3LyY9Sdllj9l5iPsXC', 'Jennifer', 'Red', 0, 1);
 INSERT INTO users(id, username, password, salt, name, surname, gender, role_id) VALUES (8, 'karina', '1d/NZaEqNgtEomytAPrwm/+QjmbudLg33oeEk77Xh88=', 'WZeBXmCI9cAz3LyY9Sdllj9l5iPsXC', 'Karina', 'Yellow', 1, 1);
+
+UPDATE users SET ENABLED = false WHERE id = 6;
 
 INSERT INTO contacts(user_id, email, phone) VALUES (1, 'andrea.test@gmail.com', NULL);
 INSERT INTO contacts(user_id, email, phone) VALUES (2, 'mario.rossi@gmail.com', NULL);
