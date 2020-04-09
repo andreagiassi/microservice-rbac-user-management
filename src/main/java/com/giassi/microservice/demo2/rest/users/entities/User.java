5f81d40f8dd333af91081b23a7de180e9db5e4c7
Andrea Giassi <andrea.giassi@travelaer.com>
2020-04-09 12:48:13 +0100

    formatting

--- src/main/java/com/giassi/microservice/demo2/rest/users/entities/User.java
+++ src/main/java/com/giassi/microservice/demo2/rest/users/entities/User.java
@@ -56,10 +56,9 @@ public class User {
     private java.time.LocalDateTime loginDt;
 
     @ManyToMany(fetch=FetchType.EAGER)
-    @JoinTable(
-            name = "users_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JoinTable(name = "users_roles",
+               joinColumns = @JoinColumn(name = "user_id"),
+               inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
 }
