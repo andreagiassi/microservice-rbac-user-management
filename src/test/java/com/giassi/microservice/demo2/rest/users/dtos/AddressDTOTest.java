2d148775b5abb4bc335e1cfa9cf27e8920f9c72f
Andrea Giassi <andrea.giassi@travelaer.com>
2021-02-08 19:27:18 +0100

    Address dto test coverage improvement

--- src/test/java/com/giassi/microservice/demo2/rest/users/dtos/AddressDTOTest.java
+++ src/test/java/com/giassi/microservice/demo2/rest/users/dtos/AddressDTOTest.java
@@ -8,6 +8,16 @@ import static org.junit.Assert.assertEquals;
 public class AddressDTOTest {
 
     @Test
+    public void getEmptyAddressDTO() {
+        AddressDTO addressDTO = new AddressDTO();
+        assertEquals(null, addressDTO.getCity());
+        assertEquals(null, addressDTO.getCountry());
+        assertEquals(null, addressDTO.getZipCode());
+        assertEquals(null, addressDTO.getAddress());
+        assertEquals(null, addressDTO.getAddress2());
+    }
+
+    @Test
     public void getAddressDTO() {
         Address address = new Address();
         address.setCity("Trieste");
